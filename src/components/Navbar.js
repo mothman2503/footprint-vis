649f7a209f91bc3e07415c43b03fcb0e851e73a6
mothman2503 <othmanghani.13@gmail.com>
2025-03-30 18:05:10 +0200

    Added Navbar for Desktop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import { useMediaQuery } from "react-responsive";
+//import { useMediaQuery } from "react-responsive";
 import MobileNavbar from "./MobileNavbar";
 import DesktopNavbar from "./DesktopNavbar";
 
