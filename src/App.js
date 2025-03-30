6f470b46153a5b597ee7ddd92924e00e2b7ffe3a
mothman2503 <othmanghani.13@gmail.com>
2025-03-30 11:57:42 +0200

    Coming soon message modified

--- src/App.js
+++ src/App.js
@@ -4,11 +4,20 @@ import './App.css';
 function App() {
   return (
     <div>
-      <header style={{backgroundColor : '#996622', color : '#000', minHeight:"100vh", maxWidth:"100vw", padding:"20px", alignItems:"center", display:"flex", flexDirection:"column", }}>
+<div style={{ height :'15px', width:'20px', alignItems:"center", display:"flex", flexDirection:"column", position:'absolute', top:'10px', left:'6px', justifyContent:'space-between'}}>
+<div style={{backgroundColor : '#000', height :'1px', width:'100%'  }} />
+<div style={{backgroundColor : '#000', height :'1px', width:'100%'  }} />
+<div style={{backgroundColor : '#000', height :'1px', width:'100%'  }} />
+</div>
+
+      <header style={{backgroundColor : '#fff', color : '#000', minHeight:"100vh", maxWidth:"100vw", padding:"20px", alignItems:"center", display:"flex", flexDirection:"column", }}>
+
         <img src={logo} className="" alt="logo" style={{maxWidth : 'calc(100% - 40px)'}}/>
+
         <h1>
           Visualise your internet footprint
         </h1>
+        <div style={{backgroundColor : '#000', height :'1px', maxWidth:'500px',  width:'calc(100% - 20px)'   }} />
 
         <h4>
           Coming soon...
