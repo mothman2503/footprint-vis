173fdf1a6674b1a79bd39a883945c95afed82516
mothman2503 <othmanghani.13@gmail.com>
2025-03-31 16:35:00 +0200

    Added Animations to Landing Page - changed some speeds

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -36,13 +36,13 @@ const [isUp, setIsUp] = useState(false);
     <div className='bg-slate-50 flex flex-col space-y-4 items-center content-start text-center w-full h-dvh pt-12 px-5'>
 
       <img src={logo} className={`max-w-full transition-transform duration-1000 ease-in-out ${
-                isUp ? "translate-y-0" : "translate-y-2.5"}`} alt="logo" />
+                isUp ? "translate-y-0" : "translate-y-1"}`} alt="logo" />
 
       <p className='font-mono font-semibold text-2xl'>
         <TypingEffect words={["Visualise", " your", " internet", " footprint!"]} speed={85} delay={50} disableSpaces={true} onComplete={() => setShowSecond(true)}/>
       </p>
 
-      <div className={`bg-slate-500 h-0.5 transition-all duration-1000 ease-in-out ${width}`} />
+      <div className={`bg-slate-500 h-0.5 transition-all duration-1000 max-w-80 ease-in-out ${width}`} />
       {showSecond && (
         <p className='font-mono font-semibold text-base'>
         <TypingEffect words={["Coming", " soon", ".", ".", "."]} speed={85} delay={50} disableSpaces={true}/>
