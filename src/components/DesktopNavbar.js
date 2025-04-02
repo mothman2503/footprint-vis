3af5f181b8ae7c94e4a95f58fb4a6f6a4e7c6988
mothman2503 <othmanghani.13@gmail.com>
2025-04-02 22:31:52 +0200

    trying to fix browser navigation bar t

--- src/components/DesktopNavbar.js
+++ src/components/DesktopNavbar.js
@@ -6,22 +6,21 @@ import { useTranslation } from "react-i18next";
 import navLinks from "../navLinks";
 
 const DesktopNavbar = () => {
+  const { t } = useTranslation();
 
-      const { t } = useTranslation(); 
-  
   return (
     <div className="bg-slate-50/85 backdrop-blur-sm max-w-full sticky top-0 flex flex-wrap space-x-8 items-center shadow-lg z-40">
-
       <img src={logo} className="max-w-20" alt="logo" />
-
 
       {navLinks.map((link, index) => (
         <p key={index}>
-          <Link className="font-mono font-semibold text-xl no-underline	hover:text-sky-950 hover:italic" to={link.path}>{t("nav."+link.label)}</Link>
+          <Link className="font-mono font-semibold text-xl no-underline hover:text-sky-950 hover:italic" to={link.path}>
+            {t("nav." + link.label)}
+          </Link>
         </p>
       ))}
 
-<LanguageSwitcher />
+      <LanguageSwitcher />
     </div>
   );
 };
